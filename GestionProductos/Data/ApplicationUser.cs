fed43688697f0ea44546eda667fbb972511ea2cf
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-20 12:25:47 -0400

    Refactor: File scoped Aplicado

--- GestionProductos/Data/ApplicationUser.cs
+++ GestionProductos/Data/ApplicationUser.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace GestionProductos.Data
-{
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+namespace GestionProductos.Data;
 
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
 }
