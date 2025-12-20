8f5b4100d6258970845fd49e0e695ca80ee426cf
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-20 12:47:09 -0400

    Feat: Creando credenciales del usuario por defecto

---
+++ GestionProductos/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GestionProductos.Data;
+
+public static class DbInitializer
+{
+    public static async Task Initialize(IServiceProvider serviceProvider)
+    {
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        await CreateUserIfNotExists(userManager, "admin", "admin@example.com", "Admin123!");
+    }
+
+    private static async Task CreateUserIfNotExists(UserManager<ApplicationUser> userManager,
+        string username, string email, string password)
+    {
+        var user = await userManager.FindByEmailAsync(email);
+        if (user == null)
+        {
+            user = new ApplicationUser
+            {
+                UserName = username,
+                Email = email,
+                EmailConfirmed = true
+            };
+            await userManager.CreateAsync(user, password);
+        }
+    }
+}
