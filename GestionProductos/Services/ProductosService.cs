0936fb500b1e5e3cd5c51a227083931f3dbc8a0e
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-20 13:16:34 -0400

    Feat: Servicio de productos trabajado

--- GestionProductos/Services/ProductosService.cs
+++ GestionProductos/Services/ProductosService.cs
@@ -1,5 +1,65 @@
-﻿namespace GestionProductos.Services;
+﻿using GestionProductos.Data;
+using GestionProductos.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
-public class ProductosService
+namespace GestionProductos.Services;
+
+public class ProductosService(IDbContextFactory<ApplicationDbContext> DbFactory)
 {
+    public async Task<bool> Guardar (Productos producto)
+    {
+        if (!await Existe (producto.ProductoId))
+        {
+            return await Insertar (producto);   
+        }
+        else
+        {
+            return await Modificar(producto);
+        }
+    }
+
+    private async Task<bool> Existe (int productoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Productos.AnyAsync(p => p.ProductoId == productoId);      
+    }
+
+    private async Task<bool> Insertar (Productos producto)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Productos.Add(producto);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar (Productos producto)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Update(producto);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Productos?> Buscar (int productoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Productos.FirstOrDefaultAsync(p => p.ProductoId == productoId);
+    }
+
+    public async Task<bool> Eliminar (int productoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Productos
+            .AsNoTracking()
+            .Where(p => p.ProductoId == productoId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Productos>> Listar (Expression<Func<Productos, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Productos
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
 }
