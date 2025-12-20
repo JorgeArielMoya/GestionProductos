8978fb8ffc090ed3676a537651c8b020ad3b163a
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-20 12:25:58 -0400

    Feat: Tablas agregadas al contexto

--- GestionProductos/Data/ApplicationDbContext.cs
+++ GestionProductos/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
+using GestionProductos.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace GestionProductos.Data
+namespace GestionProductos.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+    public DbSet<Entradas> Entradas { get; set; } 
+    public DbSet<Productos> Productos { get; set; } 
+    public DbSet<EntradasDetalles> EntradasDetalles { get; set; }
 }
