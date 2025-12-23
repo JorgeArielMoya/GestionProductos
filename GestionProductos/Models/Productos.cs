bc9c6e3a7da720556d60278b57c97032b5a38d1c
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-23 15:41:51 -0400

    Fix: Arreglando mensaje de validacion del precio, cambiado de costo a precio

--- GestionProductos/Models/Productos.cs
+++ GestionProductos/Models/Productos.cs
@@ -13,7 +13,7 @@ public class Productos
     [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe ser mayor a 0")]
     public double Costo { get; set; }
 
-    [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe ser mayor a 0")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
     public double Precio { get; set; }
     public double Existencia { get; set; }
 }
